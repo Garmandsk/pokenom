9475bd61e9787a3c11b67afa3fcac264cd11a655
Garmandask <xmiagertiarmanda@gmail.com>
2025-06-10 09:47:02 +0700

    Penambahan sprite player

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -39,14 +39,14 @@ public class Player extends Entity {
     }
 
     public void getImage(){
-        up1 = uTool.setUp("/player/boy_up_1");
-        up2 = uTool.setUp("/player/boy_up_2");
-        down1 = uTool.setUp("/player/boy_down_1");
-        down2 = uTool.setUp("/player/boy_down_2");
-        left1 = uTool.setUp("/player/boy_left_1");
-        left2 = uTool.setUp("/player/boy_left_2");
-        right1 = uTool.setUp("/player/boy_right_1");
-        right2 = uTool.setUp("/player/boy_right_2");
+        up1 = uTool.setUp("/player/cowok_up_1");
+        up2 = uTool.setUp("/player/cowok_up_2");
+        down1 = uTool.setUp("/player/cowok_down_1");
+        down2 = uTool.setUp("/player/cowok_down_2");
+        left1 = uTool.setUp("/player/cowok_left_1");
+        left2 = uTool.setUp("/player/cowok_left_2");
+        right1 = uTool.setUp("/player/cowok_right_1");
+        right2 = uTool.setUp("/player/cowok_right_2");
     }
 
     public void getAttackImage(){
